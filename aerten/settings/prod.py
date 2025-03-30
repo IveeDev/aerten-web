68b85a1444d96564ca1e3eed6dd4b9ca6cd0a1f1
IveeDev <iviidev899@gmail.com>
2025-03-30 15:34:54 +0100

    --amend

--- aerten/settings/prod.py
+++ aerten/settings/prod.py
@@ -4,6 +4,17 @@ from .common import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-local-secret-key')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aerten',  # Your local database name
+        'USER': 'root',  # Your local MySQL username
+        'PASSWORD': 'iviidev',  # Your local MySQL password
+        'HOST': 'localhost',  # Connect to MySQL locally
+        'PORT': '3306',
+    }
+}
